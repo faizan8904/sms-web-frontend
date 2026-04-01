58b2db0f335389ee87edf3f63eb681a892f4bce2
faizan <faizansyed890@gmail.com>
2026-04-01 17:48:31 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+ function App() {
+  return (
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+
+        <h1 className="text-3xl text-black font-bold ">
+         Initial Set up
+        </h1>
+      
+    </div>
+  )
+}
+
+export default App
